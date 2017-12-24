e9bc48feef0254f0249322bec64e05b27b56a672
Matthew Merritt <matthew.merritt@yahoo.com>
2017-12-23 20:07:12 -0800

    Initial Commit

---
+++ MMPreferencesWindowExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MMPreferencesWindowExample
+//
+//  Created by Matthew Merritt on 12/23/17.
+//  Copyright © 2017 Matthew Merritt. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MMPreferencesWindowExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MMPreferencesWindowExample
+//
+//  Created by Matthew Merritt on 12/23/17.
+//  Copyright © 2017 Matthew Merritt. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
